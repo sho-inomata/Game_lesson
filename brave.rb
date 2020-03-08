2488b48880597d5724d01506e091d446c0fe4d26
猪俣翔 <inomatashou@inomatashounoMacBook-Air.local>
2020-03-08 18:16:18 +0900

    ~Q3

--- brave.rb
+++ brave.rb
@@ -1,11 +1,4 @@
 require './character'
 
 class Brave < Character
-
-  def attack(monster)
-    puts "勇者の攻撃！！"
-    monster_damage = @offence - monster.defense
-    monster.hp -= monster_damage
-    puts "モンスターは#{monster_damage}のダメージを受けた！！ 残りHPは#{monster.hp}だ！！"
-  end
 end
--- monster.rb
+++ monster.rb
@@ -1,13 +1,5 @@
 require './character'
 
-
 class Monster < Character
-
-  def attack(brave)
-    puts "モンスターの攻撃！！"
-    brave_damage = @offence - brave.defense
-    brave.hp -= brave_damage
-    puts "勇者は#{brave_damage}のダメージを受けた！！ 残りHPは#{brave.hp}だ！！"
-  end
 
 end
